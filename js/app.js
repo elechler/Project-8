664e5990fc714ce4c9e9254336e9acff40b6021f
Erin Lechler <erinlechler@gmail.com>
2020-11-19 15:12:29 -0600

    intiail css and js

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// global variables
+
+let employees = [];
+
+// const​ urlAPI = ​`https://randomuser.me/api/?results=12&inc=name, picture, email, location, phone, dob &noinfo &nat=US`
+// const​ gridContainer = ​document​.querySelector(​".grid-container"​);
+
+// const​ overlay = ​document​.querySelector(​".overlay"​);
+// const​ modalContainer = ​document​.querySelector(​".modal-content"​);
+// const​ modalClose = ​document​.querySelector(​".modal-close"​);
+
+const gridContainer = document.getElementsByClassName('grid-container');
+const​ overlay = ​document​.getElementsByClassName(​'overlay');
